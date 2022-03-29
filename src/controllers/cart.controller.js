97507abcaa12806a39fd210ffd1544fc5837a2dd
mahesh-jangid <mesh.jangid@gmail.com>
2022-03-29 23:21:06 +0530

    Initial

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -30,48 +30,6 @@ router.post("/userCartItem", async (req, res) => {
   }
 });
 
-router.patch("/removeItem", async (req, res) => {
-  try {
-    let user = await Cart.findOne({ userId: req.body.userId }).lean().exec();
-    user.productId = user.productId.filter((elem, i) => {
-      if (i !== req.body.removeElemFromIndex) return elem;
-    });
-    user = await Cart.findOneAndUpdate(
-      { userId: req.body.userId },
-      { productId: user.productId },
-      { new: true }
-    );
-    return res.status(200).send(user);
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-});
-
-router.patch("/removeAllItem", async (req, res) => {
-  try {
-    let user = await Cart.findOneAndUpdate(
-      { userId: req.body.userId },
-      { productId: [] },
-      { new: true }
-    );
-    return res.status(200).send(user);
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-});
-
-router.post("/arraylength", async (req, res) => {
-  try {
-    const user = await Cart.findOne({ userId: req.body.userId }).lean().exec();
-    if (!user) {
-      return res.status(200).send({ cartArrayLength: 0 });
-    }
-    return res.status(200).send({ cartArrayLength: user.productId.length });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-});
-
 router.post("", async (req, res) => {
   try {
     const user = await Cart.findOne({ userId: req.body.userId }).lean().exec();
